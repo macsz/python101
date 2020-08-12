d875d43bf1cecedb1f0fc81ab9f99789fe36c5ae
Maciej Szankin <maciej@szankin.pl>
2020-08-11 18:33:15 -0700

    Update gitignore

--- serve_slides.sh
+++
@@ -1,6 +0,0 @@
-## If reveal.js does not load correctly clone the repository:
-# git clone https://github.com/hakimel/reveal.js.git
-# Source: https://stackoverflow.com/questions/47402384/juypter-notebooks-presentation-notes-server-error-500-403
-
-jupyter nbconvert python101.ipynb --to slides --post serve --reveal-prefix reveal.js
-
